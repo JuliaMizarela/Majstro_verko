5169f39cbb08907d19b42b14fb7c4c17cc365c07
Júlia Mizarela <juliamizarela@id.uff.br>
2022-05-14 12:55:24 -0300

    Correction to ignore

---
+++ majstro_verko/db_connection.py
@@ -0,0 +1,84 @@
+import logging
+import sys
+
+from neo4j import GraphDatabase
+from neo4j.exceptions import ServiceUnavailable
+
+
+class App:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        # Don't forget to close the driver connection when you are finished with it
+        self.driver.close()
+
+    @staticmethod
+    def enable_log(level, output_stream):
+        handler = logging.StreamHandler(output_stream)
+        handler.setLevel(level)
+        logging.getLogger("neo4j").addHandler(handler)
+        logging.getLogger("neo4j").setLevel(level)
+
+    def create_relationship(self, entity_1, entity_2, relation):
+        with self.driver.session() as session:
+            # Write transactions allow the driver to handle retries and transient errors
+            result = session.write_transaction(self._create_and_return_friendship, entity_1, entity_2, relation)
+            for row in result:
+                print(f"Created friendship between: {row['e_1']}, {row['e_2']} from {row[relation]}")
+
+    @staticmethod
+    def _create_and_return_friendship(tx, person1_name, person2_name, knows_from):
+        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
+        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
+        query = (
+            "CREATE (p1:Person { name: $person1_name }) "
+            "CREATE (p2:Person { name: $person2_name }) "
+            "CREATE (p1)-[k:KNOWS { from: $knows_from }]->(p2) "
+            "RETURN p1, p2, k"
+        )
+        result = tx.run(query, person1_name=person1_name,
+                        person2_name=person2_name, knows_from=knows_from)
+        try:
+            return [{
+                        "p1": row["p1"]["name"],
+                        "p2": row["p2"]["name"],
+                        "knows_from": row["k"]["from"]
+                    }
+                    for row in result]
+        # Capture any errors along with the query and data for traceability
+        except ServiceUnavailable as exception:
+            logging.error("{query} raised an error: \n {exception}".format(
+                query=query, exception=exception))
+            raise
+
+    def find_person(self, person_name):
+        with self.driver.session() as session:
+            result = session.read_transaction(self._find_and_return_person, person_name)
+            for row in result:
+                print("Found person: {row}".format(row=row))
+
+    @staticmethod
+    def _find_and_return_person(tx, person_name):
+        query = (
+            "MATCH (p:Person) "
+            "WHERE p.name = $person_name "
+            "RETURN p.name AS name"
+        )
+        result = tx.run(query, person_name=person_name)
+        return [row["name"] for row in result]
+
+
+if __name__ == "__main__":
+    bolt_url = "%%BOLT_URL_PLACEHOLDER%%"
+    user = "<Username for database>"
+    password = "<Password for database>"
+    App.enable_log(logging.INFO, sys.stdout)
+    app = App(bolt_url, user, password)
+    app.create_friendship("Alice", "David", "School")
+    app.find_person("Alice")
+    app.close()
+    
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
